6f6b7a7e362a1bc805b4636e192fc7206b534d37
PhamGiaHuy441 <huy108829@donga.edu.vn>
2024-11-05 10:11:42 +0700

    Add files via upload

---
+++ TUAN 2/Huy108829.java
@@ -0,0 +1,18 @@
+package buoi2;
+public class Huy108829{
+
+
+
+public static void main (String[] args){
+    System.out.println("Xin chào các bạn tân sinh viên lớp GD24A");
+    System.out.println("Xin chào các bạn tân sinh viên lớp GD24A");
+    System.out.println("25 PHẠM GIA HUY GD24A");
+    
+    int num1 = 10;
+    
+    float num2 = 5.5f;
+    System.out.printf("num1 = %d, num2 = %f", num1, num2);
+
+
+    }
+}
